49122f9238226be20c8c3c17faf17250353ef1d1
NadaLabidi1 <nada.labidi@esprit.tn>
2024-12-23 16:14:55 +0100

    constructor & setter injection, qualifier

--- src/main/java/com/nadlab/example/ExampleApplication.java
+++ src/main/java/com/nadlab/example/ExampleApplication.java
@@ -10,7 +10,9 @@ public class ExampleApplication {
 		var ctx = SpringApplication.run(ExampleApplication.class, args);
         MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
         System.out.println(myFirstService.tellAStory());
+		System.out.println(myFirstService.getJavaVersion());
+		System.out.println(myFirstService.getOsName());
+		System.out.println(myFirstService.getProperty());
 	}
-
 
 }
--- src/main/java/com/nadlab/example/MyFirstService.java
+++ src/main/java/com/nadlab/example/MyFirstService.java
@@ -1,17 +1,43 @@
 package com.nadlab.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-    private final MyFirstClass myFirstClass;
 
-    public MyFirstService(MyFirstClass myFirstClass) {
-        this.myFirstClass = myFirstClass;
+    private  MyFirstClass myFirstClass;
+    private Environment environment;
+
+    @Autowired
+    public  void setMyFirstClass( @Qualifier("myFirstBean") MyFirstClass myFirstClass){
+        this.myFirstClass= myFirstClass;
     }
 
+
+
     public String tellAStory(){
+
         return "the dependency is saying: "+ myFirstClass.sayHello();
+    }
+    public String getJavaVersion(){
+
+        return environment.getProperty("java.version");
+    }
+    public String getOsName(){
+        return environment.getProperty("os.name");
+    }
+
+    public String getProperty(){
+        return environment.getProperty("my.custom.property");
+    }
+
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
